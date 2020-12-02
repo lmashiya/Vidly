7d4776d701b478ccbb95187ff95ee830bbb39354
lmashiya <User@CODEMUNCHER>
2020-12-02 20:49:46 +0200

    Created Model Movie, a controller for movies and a View for random movie.

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    /// <summary>
+    /// A controller for movies
+    /// </summary>
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movie = new Movie(){Name = "Shrek",Id = 1};
+            return View(movie);
+        }
+    }
+}
---
+++ Vidly/Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    /// <summary>
+    /// Represents a movie.
+    /// </summary>
+    public class Movie
+    {
+        /// <summary>
+        /// Gets or sets a <see cref="string"/> representing the name of a movie.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets an <see cref="int"/> representing the id of a movie.
+        /// </summary>
+        public int Id { get; set; }
+    }
+}
