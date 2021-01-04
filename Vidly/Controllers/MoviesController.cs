0e584327ab114fa6b98866222a4eb3115bca8e11
lmashiya <User@CODEMUNCHER>
2021-01-04 16:44:07 +0200

    Added ViewModel...added Customer Model and partial view for navBar

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -15,8 +17,21 @@ namespace Vidly.Controllers
         // GET: Movies/Random
         public ActionResult Random()
         {
-            var movie = new Movie(){Name = "Shrek",Id = 1};
-            return View(movie);
+            var movie = new Movie(){Name = "Shrek"};
+
+            var customers = new List<Customer>()
+            {
+                new Customer() {Name = "Customer 1"},
+                new Customer() {Name = "Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel()
+            {
+                Customers = customers,
+                Movie = movie
+            };
+
+           return View(viewModel);
         }
     }
 }
