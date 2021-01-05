4539ddd202f4ba21421d338f09daab6095aabe7f
lmashiya <User@CODEMUNCHER>
2021-01-05 20:20:24 +0200

    Added isSubcsribedToNewsletter property and updated database.

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -7,7 +7,19 @@ namespace Vidly.Models
 {
     public class Customer
     {
+        /// <summary>
+        ///     Gets or sets an <see cref="int"/> representing the id for a customer.
+        /// </summary>
         public int Id { get; set; }
+        
+        /// <summary>
+        ///     Gets or sets a<see cref="string"/> representing the customer name.
+        /// </summary>
         public string Name { get; set; }
+        
+        /// <summary>
+        ///     Gets or sets a <see cref="bool"/> representing is customer subscribed to news letter.
+        /// </summary>
+        public bool IsSubscribedToNewsletter { get; set; }
     }
 }
