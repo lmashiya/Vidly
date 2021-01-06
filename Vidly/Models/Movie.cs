1bfa94cb8e661c3a1c0666583eef00ce538a90a0
lmashiya <User@CODEMUNCHER>
2021-01-06 11:39:35 +0200

    Changed release date to DateTime and changed date format on details html

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -23,7 +23,7 @@ namespace Vidly.Models
         /// <summary>
         ///  Gets or sets a <see cref="DateTime"/> representing release date of a movie.
         /// </summary>
-        public string ReleaseDate { get; set; }
+        public DateTime ReleaseDate { get; set; }
 
         /// <summary>
         ///  Gets or sets a <see cref="DateTime"/> representing date movie was added.
