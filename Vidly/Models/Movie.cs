7febdd7f4bbcbbbeb868bfa9b162b7688634a5d9
lmashiya <User@CODEMUNCHER>
2021-01-06 10:25:24 +0200

    Added new properties to movies and updated the database

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -19,5 +19,32 @@ namespace Vidly.Models
         /// Gets or sets an <see cref="int"/> representing the id of a movie.
         /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        ///  Gets or sets a <see cref="DateTime"/> representing release date of a movie.
+        /// </summary>
+        public string ReleaseDate { get; set; }
+
+        /// <summary>
+        ///  Gets or sets a <see cref="DateTime"/> representing date movie was added.
+        /// </summary>
+        public DateTime DateAdded { get; set; }
+
+        /// <summary>
+        ///  Gets or sets a <see cref="byte"/> representing number of stock for a movie.
+        /// </summary>
+        public byte NumberOfStock { get; set; }
+
+        /// <summary>
+        ///  Gets or sets a <see cref="Genre"/> representing genre of a movie.
+        /// </summary>
+        public Genre Genre { get; set; }
+
+        /// <summary>
+        ///  Gets or sets a <see cref="byte"/> representing a foreign key of a genre.
+        /// </summary>
+        public byte GenreId { get; set; }
+
+
     }
 }
