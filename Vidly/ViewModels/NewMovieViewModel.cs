dea6daabfb676d045463958c1c7ed6c6a1b154fe
lmashiya <User@CODEMUNCHER>
2021-01-07 11:31:29 +0200

    Added a NewMovieViewModel class

---
+++ Vidly/ViewModels/NewMovieViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.ViewModels
+{
+    public class NewMovieViewModel
+    {
+        /// <summary>
+        ///     Gets or sets a <see cref="Movie"/> representing a movie.
+        /// </summary>
+        public Movie Movie { get; set; }
+
+        /// <summary>
+        ///     Gets or sets a <see cref="IEnumerable{T}"/> representing a list of genres.
+        /// </summary>
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
