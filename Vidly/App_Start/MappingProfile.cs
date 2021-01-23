82fc0f48a436ff87e0d762ecacbde91f7bda4586
lmashiya <User@CODEMUNCHER>
2021-01-23 12:13:51 +0200

    Commented the ignore property mapping profile and created column for Delete.

--- Vidly/App_Start/MappingProfile.cs
+++ Vidly/App_Start/MappingProfile.cs
@@ -18,10 +18,10 @@ namespace Vidly.App_Start
             Mapper.CreateMap<Movie, MovieDto>();
             Mapper.CreateMap<MovieDto, Movie>();
 
-            Mapper.CreateMap<Customer, CustomerDto>().ForMember(c => c.Id,
-            opt => opt.Ignore());
-            Mapper.CreateMap<Movie, MovieDto>().ForMember(m => m.Id,
-                    opt => opt.Ignore());
+            //Mapper.CreateMap<Customer, CustomerDto>().ForMember(c => c.Id,
+            //opt => opt.Ignore());
+            //Mapper.CreateMap<Movie, MovieDto>().ForMember(m => m.Id,
+            //        opt => opt.Ignore());
         }
     }
 }
