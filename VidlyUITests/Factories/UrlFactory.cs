e1b6b174e54f4c23d2af2d3debd31c95ec30c628
lmashiya <User@CODEMUNCHER>
2021-01-30 21:30:02 +0200

    Implementing POM(Page Object Model (POM) is a design pattern, popularly used in test automation that creates Object Repository for web UI elements. The advantage of the model is that it reduces code duplication and improves test maintenance.)

---
+++ VidlyUITests/Factories/UrlFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VidlyUITests.Factories
+{
+    public static class UrlFactory
+    {
+        public static string LoginPageUrl => "hello";
+    }
+}
---
+++ VidlyUITests/Tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+using System.Runtime.InteropServices.ComTypes;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using VidlyUITests.Factories;
+
+namespace VidlyUITests
+{
+    [TestFixture]
+    public class Tests 
+    {
+        private IWebDriver _driver;
+
+        [Test]
+        public void OpenLoginScreen()
+        {
+            using (_driver)
+            {
+                
+            }
+        }
+    }
+}
